c70d0a3ec5da69585b8df8e4527ada048efab498
Luis Oswaldo Rodríguez López <rodriguez.ruisu@gmail.com>
2024-03-13 19:44:04 -0600

    xdccxv

--- client/src/Screens/Visualizar/SA_visualizar.jsx
+++ client/src/Screens/Visualizar/SA_visualizar.jsx
@@ -3,7 +3,6 @@ import NarBar from '../NarBar.js/NarBar';
 import axios from 'axios';
 import "./SA_visualizar.css";
 import { DataGrid } from '@mui/x-data-grid';
-//import { makeStyles } from '@material-ui/core/styles';
 import Snackbar from '@mui/material/Snackbar';
 import MuiAlert from '@mui/material/Alert';
 import Button from '@mui/material/Button';
@@ -12,6 +11,8 @@ import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 const SA_visualizar = () => {
   const [data, setData] = useState([]);
+  const [sedes, setSedes] = useState([]);
+  const [areas, setAreas] = useState([]); // Nuevo estado para almacenar las áreas disponibles
   const [sedeSeleccionada, setSedeSeleccionada] = useState('');
   const [areaSeleccionada, setAreaSeleccionada] = useState('');
   const [estadoSeleccionado, setEstadoSeleccionado] = useState('');
@@ -19,7 +20,6 @@ const SA_visualizar = () => {
   const [numeroEmpleadoBuscado, setNumeroEmpleadoBuscado] = useState('');
   const [openAlert, setOpenAlert] = useState(false);
   const [deletedUserName, setDeletedUserName] = useState('');
-  
 
   const handleCloseAlert = (event, reason) => {
     if (reason === 'clickaway') {
@@ -36,6 +36,24 @@ const SA_visualizar = () => {
       })
       .catch(error => {
         console.error('Error al obtener datos:', error);
+      });
+
+    axios.get('http://localhost:3000/sedes_areas') // Obtener la lista de sedes disponibles
+      .then(response => {
+        console.log(response.data);
+        setSedes(response.data.data.map(sede => sede.Nombre)); // Almacenar solo los nombres de sedes
+      })
+      .catch(error => {
+        console.error('Error al obtener las sedes:', error);
+      });
+
+    axios.get('http://localhost:3000/tipos') // Obtener la lista de tipos de áreas disponibles
+      .then(response => {
+        console.log(response.data);
+        setAreas(response.data.data.map(area => area.Tipo)); // Almacenar solo los tipos de áreas
+      })
+      .catch(error => {
+        console.error('Error al obtener los tipos de áreas:', error);
       });
   }, []);
 
@@ -71,82 +89,76 @@ const SA_visualizar = () => {
       })
       .catch(err => console.log(err))
   };
+
   const columns = [
-    { field: 'Nombre', headerName: 'Nombre', width: 150,  },
-    { field: 'Area', headerName: 'Área', width: 100,  },
-    { field: 'Sede', headerName: 'Sede', width: 100, },
-    { field: 'Status', headerName: 'Estado', width: 100,  },
-    { field: 'Tipo', headerName: 'Tipo', width: 150,  },
-    { field: 'Numero_Empleado', headerName: 'Num. Empleado', width: 150, },
-    { field: 'Sexo', headerName: 'Sexo', width: 150,  },
+    { field: 'Nombre', headerName: 'Nombre', width: 150 },
+    { field: 'Area', headerName: 'Área', width: 100 },
+    { field: 'Sede', headerName: 'Sede', width: 100 },
+    { field: 'Status', headerName: 'Estado', width: 100 },
+    { field: 'Tipo', headerName: 'Tipo', width: 150 },
+    { field: 'Numero_Empleado', headerName: 'Num. Empleado', width: 150 },
+    { field: 'Sexo', headerName: 'Sexo', width: 150 },
     {
       field: 'actions',
       headerName: 'Acciones',
       width: 500,
       renderCell: (params) => (
         <div>
-
-          <Button className="actions-button"  variant="outlined"  startIcon={<CloudUploadIcon />}onClick={() => window.location.href=`/sa-Modificar/${params.row.id}`}>Subir Doct</Button>
-          <Button className="actions-button"  variant="outlined"  onClick={() => window.location.href=`/sa-Modificar/${params.row.id}`}>Modificar/Ver Datos</Button>
+          <Button className="actions-button" variant="outlined" startIcon={<CloudUploadIcon />} onClick={() => window.location.href=`/sa-Modificar/${params.row.id}`}>Subir Doct</Button>
+          <Button className="actions-button" variant="outlined" onClick={() => window.location.href=`/sa-Modificar/${params.row.id}`}>Modificar/Ver Datos</Button>
           <Button onClick={() => handleDelete(params.row.id, params.row.Nombre)} variant="outlined" color="error" startIcon={<DeleteIcon />}>Eliminar</Button>
         </div>
       ),
-      
     },
   ];
 
+  const usuariosFiltrados = data.filter(user => (
+    (sedeSeleccionada === '' || user.Sede === sedeSeleccionada) &&
+    (areaSeleccionada === '' || user.Area === areaSeleccionada) &&
+    (estadoSeleccionado === '' || user.Status === estadoSeleccionado) &&
+    (generoSeleccionado === '' || user.Tipo === generoSeleccionado) &&
+    (numeroEmpleadoBuscado === '' || (user.Numero_Empleado && user.Numero_Empleado.toString().includes(numeroEmpleadoBuscado)))
+  ));
 
-  const usuariosFiltrados = data.filter(user => {
-    return (
-      (sedeSeleccionada === '' || user.Sede === sedeSeleccionada) &&
-      (areaSeleccionada === '' || user.Area === areaSeleccionada) &&
-      (estadoSeleccionado === '' || user.Status === estadoSeleccionado) &&
-      (generoSeleccionado === '' || user.Tipo === generoSeleccionado) &&
-      (numeroEmpleadoBuscado === '' || (user.Numero_Empleado && user.Numero_Empleado.toString().includes(numeroEmpleadoBuscado)))
-    );
-  });
-
-
-  const usuarios = usuariosFiltrados.map((usuario, index) => {
-    return { ...usuario, id: usuario._id || index };
-  });
+  const usuarios = usuariosFiltrados.map((usuario, index) => ({
+    ...usuario,
+    id: usuario._id || index
+  }));
 
   return (
     <>
       <NarBar />
       <div className="v141_16">
-        
-      <div style={{ height: 400, width: '100%',marginTop: 100 }}>
-        <DataGrid
-          rows={usuarios}
-          columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5, 10, 20]}
-          checkboxSelection
-          
-        />
-      </div>
-
+        <div style={{ height: 400, width: '100%', marginTop: 100 }}>
+          <DataGrid
+            rows={usuarios}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5, 10, 20]}
+            checkboxSelection
+          />
+        </div>
       </div>
 
       <select className="v141_72 v141_73" value={sedeSeleccionada} onChange={handleSedeChange}>
-        <option  value=''>Seleccione una Sede</option>
-        <option value="Leon">Leon</option>
-        <option value="Salamanca">Salamanca</option>
+        <option value=''>Seleccione una Sede</option>
+        {sedes.map((sede, index) => (
+          <option key={index} value={sede}>{sede}</option>
+        ))}
       </select>
            
       <select className="v141_74 v141_75" value={areaSeleccionada} onChange={handleAreaChange}>
-        <option  value=''>Seleccione una Area</option>
-        <option value="1">1</option>
-        <option value="2">2</option>
+        <option value=''>Seleccione una Área</option>
+        {areas.map((area, index) => (
+          <option key={index} value={area}>{area}</option>
+        ))}
       </select>
       
       <select className="v141_76 v141_77" value={estadoSeleccionado} onChange={handleEstadoChange}>
-        <option  value=''>Seleccione un Estado</option>
+        <option value=''>Seleccione un Estado</option>
         <option value="Activo">Activo</option>
         <option value="Inactivo">Inactivo</option>
       </select>
-
       <select className="v141_78 v141_79" value={generoSeleccionado} onChange={handleGeneroChange}>
         <option  value=''>Seleccione un Tipo</option>
         <option value="Empleado">Empleado</option>
